22c81234c0f681bac54de27c1828d380e253464e
Dumitru Tronciu <dumitru.tronciu@gmail.com>
2023-10-17 11:56:28 +0300

    Lab3

--- lab3/aplicatie1/locatie.js
+++ lab3/aplicatie1/locatie.js
@@ -5,7 +5,7 @@ function on_success(position) {
     document.getElementById("acc").innerHTML = "Accuracy = " + position.coords.accuracy;
     document.getElementById("altitude").innerHTML = "Altitude = " + position.coords.altitude;
     var latlon = position.coords.latitude + "," + position.coords.longitude;
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&key=AIzaSyBzkimiDmD2lrz-wltbD0tNBWW4U03CmT8";
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&key=API_KEY";
     document.getElementById("map").innerHTML = "<img src='" + img_url + "'>";
 }
 function on_error(e) {
